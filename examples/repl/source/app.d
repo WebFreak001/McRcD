ff15c19ea0976c1c170f0468e5e9cff6e86a5bdd
WebFreak001 <gh@webfreak.org>
2024-01-09 21:28:59 +0100

    modernize code base

--- examples/repl/source/app.d
+++ examples/repl/source/app.d
@@ -13,52 +13,45 @@ void main(string[] args)
 
 	if(args.length > 1)
 	{
-		if(args[1].indexOf(':') != -1)
+		auto parts = args[1].findSplit(":");
+		if(parts[2].length)
 		{
-			string[] parts = args[1].split(':');
-			if(parts.length > 2)
+			host = parts[0];
+			try
 			{
-				writeln("Malformed IP!");
-				return;
+				port = parse!ushort(parts[2]);
 			}
-			else
+			catch(ConvException e)
 			{
-				host = parts[0];
-				try
-				{
-					port = parse!ushort(parts[1]);
-				}
-				catch(Exception e)
-				{
-					writeln("Malformed Port!");
-					return;
-				}
+				stderr.writeln("Malformed Port!");
+				return;
 			}
 		}
 		else
 		{
-			host = args[1];
+			host = parts[0];
 		}
+
 		if(args.length >= 2)
 		{
 			password = args[2];
 		}
 	}
 
-	writefln("Attempting to connect to %s:%s...", host, port);
+	stderr.writefln("Attempting to connect to %s:%s...", host, port);
 	try
 	{
 		rcon.connect(host, port);
 	}
 	catch(Exception e)
 	{
-		writeln("Couldn't connect!");
-		writeln(e);
+		stderr.writeln("Couldn't connect!");
+		stderr.writeln(e);
 		return;
 	}
 	scope(exit) rcon.disconnect();
 
-	writeln("Logging in...");
+	stderr.writeln("Logging in...");
 	rcon.login(password);
 
 	while(true)
